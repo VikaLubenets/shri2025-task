1d9c6d31cad6398108eed43724ad69c702eacfc9
VikaLubenets <lubenetcvika@gmail.com>
2025-07-05 15:59:33 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/shri2025-task/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/shri2025-task/' : '/',
   plugins: [react()],
-})
+}))
